4b068b50bc08a4c2702ec3d9856e0f8ed25b46fc
iagosm <iagosousa201486@gmail.com>
2024-03-16 10:46:16 -0300

    Finalizando api do projeto

--- script.js
+++ script.js
@@ -56,20 +56,106 @@ function updateCartModal(){
 
   cart.forEach(item =>  {
     const cartItemElement = document.createElement("div");
+    cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
     cartItemElement.innerHTML = `
-    <div>
+    <div class="flex items-center justify-between">
       <div>
-        <p>${item.name}</p>
-        <p>${item.quantity}</p>
-        <p>R$ ${item.price}</p>
+        <p class="font-medium">${item.name}</p>
+        <p>Qtd: ${item.quantity}</p>
+        <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
       </div>
-      <div>
-        <button>
-          Remover
-        </button>
-      </div>
+      <button class="remove-from-cart-btn" data-name="${item.name}">
+        Remover
+      </button>
     </div>
     `;
+    total += item.price * item.quantity;
     cartItemsContainer.appendChild(cartItemElement);
   })
+  cartTotal.textContent = total.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  });
+  cartCounter.innerHTML = cart.length;
+}
+
+cartItemsContainer.addEventListener("click", function(event) {
+  if(event.target.classList.contains("remove-from-cart-btn")) {
+    const name = event.target.getAttribute("data-name");
+    removeItemCart(name);
+  }
+});
+
+function removeItemCart(name) {
+  const index = cart.findIndex(item => item.name == name);
+  if(index !== -1) {
+    const item = cart[index];
+    if(item.quantity > 1) {
+      item.quantity -= 1;
+      updateCartModal();
+      return;
+    }
+  }
+  cart.splice(index, 1);
+  updateCartModal();
+}
+
+addressInput.addEventListener("input", function(event) {
+  let inputValue = event.target.value;
+  if(inputValue !== "") {
+    addressInput.classList.remove("border-red-500");
+    addressWarn.classList.add("hidden"); 
+  }
+});
+
+checkoutBtn.addEventListener("click", function(){
+  const isOpen = checkRestaurantOpen();
+  if(!isOpen) {
+    Toastify({
+      text: "Ops ... restaurante Fechado no momento",
+      duration: 3000,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "#ef4444",
+      },
+      
+    }).showToast();
+    return;
+  }
+  if(cart.length === 0) return;
+  if(addressInput.value === "") {
+    addressWarn.classList.remove("hidden"); 
+    addressInput.classList.add("border-red-500");
+    return;
+  }
+  const cartItems = cart.map((item) => {
+    return (
+      `${item.name} Quantidade: (${item.quantity}) Preço: R$ ${item.price} |`
+    )
+  }).join("");
+  const message = encodeURIComponent(cartItems);
+  const phone = "71996099120";
+  const address = encodeURIComponent(addressInput.value);
+  window.open(`https://wa.me/${phone}?text=${message} Endereço: ${address}`, "_blank");
+  
+});
+
+function checkRestaurantOpen() {
+  const data = new Date();
+  const hora = data.getHours();
+  return hora >= 18 && hora < 22;
+}
+
+const spanItem= document.getElementById("date-span"); 
+const isOpen = checkRestaurantOpen();
+
+if(isOpen) {
+  spanItem.classList.remove("bg-red-500");
+  spanItem.classList.add("bg-green-600");
+} else{
+  spanItem.classList.add("bg-red-500");
+  spanItem.classList.remove("bg-green-600");
 }
