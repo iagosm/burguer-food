659ea1efdfa2ed4bc93891bea457c1d6050de824
iagosm <iagosousa201486@gmail.com>
2024-03-16 02:39:24 -0300

    Adicionando funções no carrinho

---
+++ script.js
@@ -0,0 +1,75 @@
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn");
+
+let cart = [];
+
+cartBtn.addEventListener("click", function() {
+  cartModal.style.display = "flex";
+});
+
+cartModal.addEventListener("click", function(event) {
+  if(event.target === cartModal) {
+    cartModal.style.display = "none";
+  }
+})
+
+closeModalBtn.addEventListener("click", function() {
+  cartModal.style.display = "none";
+})
+
+menu.addEventListener("click", function(event) {
+  console.log(event.target)
+  let parentButton = event.target.closest(".add-to-cart-btn");
+  if(parentButton) {
+    const name = parentButton.getAttribute("data-name");
+    const price = parseFloat(parentButton.getAttribute("data-price"));
+    addToCart(name, price)
+  }
+  // let parentButton = event.target.closest("i");
+})
+
+function addToCart(name, price) {
+  const existingItem = cart.find(item => item.name === name);
+  if(existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    cart.push({
+      name, 
+      price,
+      quantity: 1,
+    });
+  }
+  updateCartModal();
+}
+
+function updateCartModal(){
+  cartItemsContainer.innerHTML = "";
+  let total = 0;
+
+  cart.forEach(item =>  {
+    const cartItemElement = document.createElement("div");
+    cartItemElement.innerHTML = `
+    <div>
+      <div>
+        <p>${item.name}</p>
+        <p>${item.quantity}</p>
+        <p>R$ ${item.price}</p>
+      </div>
+      <div>
+        <button>
+          Remover
+        </button>
+      </div>
+    </div>
+    `;
+    cartItemsContainer.appendChild(cartItemElement);
+  })
+}
